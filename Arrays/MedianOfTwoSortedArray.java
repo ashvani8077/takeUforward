56557de85f827cd84dacc8a454417f67871df132
Ashvani Sharma <panditashuabvp@gmail.com>
2025-03-02 21:18:16 +0530

    Median of two sorted array

---
+++ Arrays/MedianOfTwoSortedArray.java
@@ -0,0 +1,26 @@
+//import  java.util.*;
+//
+//package Arrays;
+//
+//class MedianOfTwoSortedArray {
+//    public double findMedianSortedArrays(int[] arr1, int[] arr2) {
+//        ArrayList<Integer>li=new ArrayList<>();
+//        for(int i=0;i<arr1.length;i++){
+//            li.add(arr1[i]);
+//        }
+//        for(int i=0;i<arr2.length;i++){
+//            li.add(arr2[i]);
+//        }
+//        Collections.sort(li);
+//        int len=li.size();
+//        if(len%2==1){
+//            int mid=len/2;
+//            return (double)(li.get(mid));
+//        }
+//        int mid=len/2;
+//        int mid2=mid-1;
+//        int  a=li.get(mid);
+//        int b=li.get(mid2);
+//        return (a + b) / 2.0;
+//    }
+//}
