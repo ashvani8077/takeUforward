1f55890bfab7c1467392120030227e41ce16cc8c
Ashvani Sharma <panditashuabvp@gmail.com>
2025-04-22 22:31:50 +0530

    Kth largest element

---
+++ Heap/KthLargestElement.java
@@ -0,0 +1,27 @@
+package Heap;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class KthLargestElement {
+    public static void main(String[] args) {
+        int arr[]={7, 10, 4, 3, 20, 15};
+        int k=3;
+        System.out.println(solution(k,arr));
+    }
+    public static int solution(int k,int arr[]){
+        int l=0;
+        int r=arr.length-1;
+        PriorityQueue<Integer>pq=new PriorityQueue<>();
+        for(int i=0;i<k;i++){
+            pq.add(arr[i]);
+        }
+        for(int i=k;i<=r;i++){
+            if(arr[i]>pq.peek()){
+                pq.poll();
+                pq.add(arr[i]);
+            }
+        }
+        return pq.poll();
+    }
+}
