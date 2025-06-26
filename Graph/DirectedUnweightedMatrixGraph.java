c6e79921eacd037d92f6dd6f050aed48594bb16b
Ashvani Sharma <panditashuabvp@gmail.com>
2025-06-26 10:44:54 +0530

    Direct unweighted represent using matrix

---
+++ Graph/DirectedUnweightedMatrixGraph.java
@@ -0,0 +1,31 @@
+package Graph;
+import java.util.*;
+import java.util.Scanner;
+
+public class DirectedUnweightedMatrixGraph {
+
+    //Adjacency matrix
+    //directed unweighted graph
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int vertex=sc.nextInt();
+        int edges= sc.nextInt();
+        int arr[][]=new int[vertex][vertex];
+        // Arrays.fill(arr,0);
+        int u,v, weight;
+        for (int i=0;i<edges;i++){
+            u= sc.nextInt();
+            v=sc.nextInt();
+            arr[u][v]=1;
+        }
+
+        //print
+        for (int i=0;i<vertex;i++){
+            for (int j=0;j<vertex;j++){
+                System.out.print(arr[i][j]+" ");
+
+            }
+            System.out.println();
+        }
+    }
+}
